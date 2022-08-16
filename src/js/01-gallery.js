c8811245388efa1ff5f9ce41824d3490a43c34a4
Valeriy Nizovtsev <nizucer123@gmail.com>
2022-08-16 22:01:14 +0300

    task 1 completed

---
+++ src/js/01-gallery.js
@@ -0,0 +1,26 @@
+import SimpleLightbox from 'simplelightbox';
+import { galleryItems } from './gallery-items.js';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+// Change code below this line
+const gallery = document.querySelector('.gallery');
+
+function renderGallery() {
+  const galleryItemsHtml = galleryItems.map(({ description, original, preview }) => {
+    const galleryItem = `      
+      <a class="gallery__item" href="${original}">
+        <img class="gallery__image" src="${preview}" alt="${description}" />
+      </a>      
+    `;
+
+    return galleryItem;
+  });
+
+  gallery.insertAdjacentHTML('beforeend', galleryItemsHtml.join(''));
+}
+
+renderGallery();
+
+new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
