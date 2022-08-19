457030b83853393aa900d4228fcf4ab17ab0568d
Valeriy Nizovtsev <nizucer123@gmail.com>
2022-08-19 23:48:31 +0300

    Homework 8, task 2,3

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,12 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
+
+const player = new Player('vimeo-player');
+
+player.setCurrentTime(localStorage.getItem('videoplayer-current-time') || 0);
+
+player.on('timeupdate', throttle(data => {
+  localStorage.setItem('videoplayer-current-time', data.seconds);
+}, 1000));
+
 
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,33 @@
+import throttle from 'lodash.throttle';
 
+const form = document.querySelector('.feedback-form');
+const formData = localStorage.getItem('feedback-form-state');
+
+if (formData) {
+  const { email, message } = JSON.parse(formData);
+
+  form.elements.email.value = email;
+  form.elements.message.value = message;
+}
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+
+  localStorage.clear();
+
+  console.log({
+    email: e.currentTarget.elements.email.value,
+    message: e.currentTarget.elements.message.value,
+  });
+
+  form.reset();
+});
+
+const onInput = throttle((e) => {
+  const email = form.elements.email.value;
+  const message = form.elements.message.value;
+
+  localStorage.setItem('feedback-form-state', JSON.stringify({ email, message }));
+}, 500);
+
+form.addEventListener('input', onInput);
